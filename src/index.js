90b4e72c0298f65b933859f4d7b1757b722c6d99
Jesse Wolgamott <jesse@comal.io>
2017-10-19 15:09:26 -0500

    initial 1.0 extraction of HoverImage

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class HoverImage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      src: this.props.src
+    }
+  }
+
+  handleClick = e => {
+    if (this.props.disabled == true) {
+    } else if (!!this.props.onClick) {
+      this.props.onClick(e)
+    }
+  }
+
+  mouseOver = () => {
+    this.setState({ src: this.props.hoverSrc })
+  }
+
+  mouseOut = () => {
+    this.setState({ src: this.props.src })
+  }
+
+  render() {
+    return (
+      <img
+        src={this.state.src}
+        style={this.props.style}
+        onMouseOver={this.mouseOver}
+        onMouseOut={this.mouseOut}
+        onClick={this.handleClick}
+      />
+    )
+  }
+}
+HoverImage.propTypes = {
+  hoverSrc: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired
+}
+
+export default HoverImage

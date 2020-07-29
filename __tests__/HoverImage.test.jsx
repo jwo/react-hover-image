9c81a5984dc8a76905f32c182f991da607f4a6de
Jesse Wolgamott <jesse_wolgamott@eogresources.com>
2020-07-29 08:07:37 -0500

    Extract expected for code-climate

--- __tests__/HoverImage.test.jsx
+++ __tests__/HoverImage.test.jsx
@@ -1,58 +1,74 @@
-import React from 'react';
-import Enzyme, { shallow, mount, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow, mount, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
-import HoverImage from '../src';
+import HoverImage from "../src";
 
-test('Displays initial src image', () => {
-  const wrapper = mount( <HoverImage src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />)
+const EXPECTED_ELEMENT = (
+  <HoverImage src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />
+);
+test("Displays initial src image", () => {
+  const wrapper = mount(EXPECTED_ELEMENT);
   expect(wrapper.state().src).toBe("/img/first.png");
-})
+});
 
-test('Changes to hoverSrc on mouseOver', () => {
-  const wrapper = mount( <HoverImage src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />)
-  wrapper.find("img").simulate('mouseover');
+test("Changes to hoverSrc on mouseOver", () => {
+  const wrapper = mount(EXPECTED_ELEMENT);
+  wrapper.find("img").simulate("mouseover");
   expect(wrapper.state().src).toBe("/img/first-hover.png");
-})
+});
 
-test('When given an onClick, it gets called when clicked', () => {
-
+test("When given an onClick, it gets called when clicked", () => {
   const doneChange = jest.fn();
 
   const wrapper = shallow(
-    <HoverImage onClick={doneChange} src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />
+    <HoverImage
+      onClick={doneChange}
+      src={"/img/first.png"}
+      hoverSrc={"/img/first-hover.png"}
+    />
   );
-  wrapper.find('img').simulate('click');
+  wrapper.find("img").simulate("click");
   expect(doneChange).toBeCalled();
 });
 
-test('When given an onClick, it does not get called when disabled', () => {
-
+test("When given an onClick, it does not get called when disabled", () => {
   const doneChange = jest.fn();
-  const disabled = true
+  const disabled = true;
 
-  const wrapper = shallow(<HoverImage disabled={disabled}  onClick={doneChange} src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />)
-  wrapper.find('img').simulate('click');
+  const wrapper = shallow(
+    <HoverImage
+      disabled={disabled}
+      onClick={doneChange}
+      src={"/img/first.png"}
+      hoverSrc={"/img/first-hover.png"}
+    />
+  );
+  wrapper.find("img").simulate("click");
 
   expect(doneChange).not.toBeCalled();
 });
 
-test('No errors if click, but no onClick provided', () => {
-
-  const wrapper = shallow(<HoverImage src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />)
-  expect( () => {
-    wrapper.find('img').simulate('click')
-  }).not.toThrowError()
-
+test("No errors if click, but no onClick provided", () => {
+  const wrapper = shallow(EXPECTED_ELEMENT);
+  expect(() => {
+    wrapper.find("img").simulate("click");
+  }).not.toThrowError();
 });
 
-test('Matches snapshot', () => {
-
+test("Matches snapshot", () => {
   const doneChange = jest.fn();
-  const disabled = false
+  const disabled = false;
 
-  const wrapper = shallow(<HoverImage disabled={disabled}  onClick={doneChange} src={"/img/first.png"} hoverSrc={"/img/first-hover.png"} />)
+  const wrapper = shallow(
+    <HoverImage
+      disabled={disabled}
+      onClick={doneChange}
+      src={"/img/first.png"}
+      hoverSrc={"/img/first-hover.png"}
+    />
+  );
 
-  expect(wrapper).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot();
 });
